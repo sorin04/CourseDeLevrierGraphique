9b06c4d01d677e1afdd2c04ca00fbe0e03256293
sorin04 <sorin.pirgari@lyceeastier.com>
2024-11-29 20:19:31 +0100

    Connecter button commencer et label pour les num

--- CourseDeLevrierGraphique/Form1.Designer.cs
+++ CourseDeLevrierGraphique/Form1.Designer.cs
@@ -29,19 +29,67 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            textBox1justnumero = new TextBox();
+            label1nombre_a_saisir = new Label();
+            label1 = new Label();
+            button_Start = new Button();
             SuspendLayout();
+            // 
+            // textBox1justnumero
+            // 
+            textBox1justnumero.Location = new Point(385, 114);
+            textBox1justnumero.Name = "textBox1justnumero";
+            textBox1justnumero.Size = new Size(88, 31);
+            textBox1justnumero.TabIndex = 0;
+            textBox1justnumero.TextChanged += textBox1justnumero_TextChanged;
+            // 
+            // label1nombre_a_saisir
+            // 
+            label1nombre_a_saisir.AutoSize = true;
+            label1nombre_a_saisir.Location = new Point(347, 96);
+            label1nombre_a_saisir.Name = "label1nombre_a_saisir";
+            label1nombre_a_saisir.Size = new Size(0, 25);
+            label1nombre_a_saisir.TabIndex = 1;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(98, 114);
+            label1.Name = "label1";
+            label1.Size = new Size(221, 25);
+            label1.TabIndex = 2;
+            label1.Text = "Choisir Levrier entre 2 et 5:";
+            // 
+            // button_Start
+            // 
+            button_Start.Location = new Point(319, 336);
+            button_Start.Name = "button_Start";
+            button_Start.Size = new Size(229, 34);
+            button_Start.TabIndex = 3;
+            button_Start.Text = "Commencer la course";
+            button_Start.UseVisualStyleBackColor = true;
+            button_Start.Click += button_Start_Click;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
             BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
-            ClientSize = new Size(878, 544);
+            ClientSize = new Size(901, 580);
+            Controls.Add(button_Start);
+            Controls.Add(label1);
+            Controls.Add(label1nombre_a_saisir);
+            Controls.Add(textBox1justnumero);
             Name = "Form1";
             Text = "Form1";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private TextBox textBox1justnumero;
+        private Label label1nombre_a_saisir;
+        private Label label1;
     }
 }
--- CourseDeLevrierGraphique/Form1.cs
+++ CourseDeLevrierGraphique/Form1.cs
@@ -6,36 +6,32 @@ namespace CourseDeLevrierGraphique
 {
     public partial class Form1 : Form
     {
-        private Button boutonStart;
+        private TextBox txtNombreLevrier;
+        private Button button_Start;
+
 
         public Form1()
         {
             InitializeComponent();
-            InitialiserInterface();
             this.FormBorderStyle = FormBorderStyle.FixedDialog;
             this.MaximizeBox = false;
             this.MinimizeBox = false;
+
         }
 
-        private void InitialiserInterface()
-        {
-            this.Text = "Course de Levriers";
-            this.ClientSize = new Size(800, 400);
 
-            boutonStart = new Button
-            {
-                Text = "Commencer la course",
-                Location = new Point((this.ClientSize.Width / 2) - 100, (this.ClientSize.Height / 2) - 20),
-                AutoSize = true
-            };
-            boutonStart.Click += BtnStart_Click;
-            this.Controls.Add(boutonStart);
+
+
+        private void button_Start_Click(object sender, EventArgs e)
+        {
+
         }
 
-        private void BtnStart_Click(object sender, EventArgs e)
+        private void textBox1justnumero_TextChanged(object sender, EventArgs e)
         {
-            CourseForm courseForm = new CourseForm(5);
-            courseForm.ShowDialog();
+
         }
     }
+
+
 }
